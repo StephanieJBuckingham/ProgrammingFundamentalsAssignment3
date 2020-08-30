2984b026ced6be0228035842adf1ee5c58d27230
StephanieJBuckingham <68093533+StephanieJBuckingham@users.noreply.github.com>
2020-08-30 00:21:27 -0400

    Update Accuracy

--- Assignment3/src/NearestNeighbor.java
+++ Assignment3/src/NearestNeighbor.java
@@ -1,8 +1,3 @@
-import java.util.Scanner;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
-
 /* Stephanie Buckingham
  * Programming Fundamentals
  * Summer Session II
@@ -15,144 +10,163 @@ import java.util.ArrayList;
  * classified and the accuracy is computed.  
  */
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class NearestNeighbor {
 
 	public static void main(String[] args) throws FileNotFoundException {
 
-		// method to print program header
+		// Method to print program header
 		printHeader();
 
-		// create scanner to import name of file
+		// Create scanner to import name of file
 		Scanner inputScan = new Scanner(System.in);
 
-		// prompt user for name of training file
+		// Prompt user for name of training file
 		System.out.print("Enter the name of the training file: ");
-		File trainFile = new File("./src/" + inputScan.next());
-		Scanner trainScan = new Scanner(trainFile);
+		File trainFile = new File(inputScan.next());
 
-		// prompt user for name of testing file
+		// Prompt user for name of testing file
 		System.out.print("Enter the name of the testing file: ");
-		File testFile = new File("./src/" + inputScan.next());
-		Scanner testScan = new Scanner(testFile);
-		inputScan.close();
+		File testFile = new File(inputScan.next());
 
-		// create 2d double array for values
+		// Create 2d double array for values
 		double[][] trainingValues = new double[75][4];
 		double[][] testingValues = new double[75][4];
 
-		// create string array for classes
+		// Create string array for classes
 		String[] trainingClasses = new String[75];
 		String[] testingClasses = new String[75];
-
-		// create array for the prediction classes
+		
+		// Create the predictions array to hold the predicted values
 		String[] predictions = new String[75];
 
-		// call method to copy the measurements in files to array
-		trainingValues = createValuesArray(trainScan, trainingValues);
-		testingValues = createValuesArray(testScan, testingValues);
+		// Make scanners to scan in test values and classes
+		Scanner testClassScan = new Scanner(testFile);
+		Scanner testValuesScan = new Scanner(testFile);
 
-		// call method to copy species names in files to array
-		trainingClasses = createSpeciesArray(trainScan, trainingClasses);
-		testingClasses = createSpeciesArray(testScan, testingClasses);
+		// Make scanners to scan in train values and classes
+		Scanner trainClassScan = new Scanner(trainFile);
+		Scanner trainValuesScan = new Scanner(trainFile);
 
-		// print space between
-		System.out.println();
+		// Invoke createClassesArray method to copy classes into the arrays
+		testingClasses = createClassesArray(testClassScan, testingClasses);
+		trainingClasses = createClassesArray(trainClassScan, trainingClasses);
 
-		// loop that goes through each row and uses nearest neighbor method to predict
-		// classes and then records these predictions in new array
+		// Invoke createValuesArray method to the copy the doubles into the arrays
+		testingValues = createValuesArray(testValuesScan, testingValues);
+		trainingValues = createValuesArray(trainValuesScan, trainingValues);
+
+		// Iterates through each row and invokes the getNearestNeighbor method to find
+		// the class label of the closest training example and then inputs these
+		// predictions into an array
+		int nearestneighbor = 0;
 		for (int i = 0; i < 75; i++) {
-			int nearestNeighbor = shortestDistance(trainingValues[i][0], trainingValues[i][1], trainingValues[i][2],
-					trainingValues[i][3], trainingValues);
-
-			predictions[i] = trainingClasses[nearestNeighbor];
+			nearestneighbor = getNearestNeighbor(i, testingValues, trainingValues);
+			predictions[i] = trainingClasses[nearestneighbor];
 		}
-		// call method to print results
-		printResults(predictions,testingClasses);
-		
+
+		// Invoke printResults method to print the examples true label, predicted label,
+		// and overall accuracy of model
+		printResults(predictions, testingClasses);
+
+		// Close Scanners to prevent resource leaks
+		inputScan.close();
+		testClassScan.close();
+		trainClassScan.close();
+		testValuesScan.close();
+		trainValuesScan.close();
 	}
 
-	// method to print program header
-	public static void printHeader() {
-		// print program label
+	// Method that prints program header
+	static void printHeader() {
 		System.out.println("Programming Fundamentals");
 		System.out.println("NAME: Stephanie Buckingham");
 		System.out.println("PROGRAMMING ASSIGNMENT 3\n");
 	}
 
-	// method to take classes from scanner and input them into a string array
-	static String[] createSpeciesArray(Scanner scanClass, String[] classes) {
+	// Method to take classes from scanner and input them into a string array
+	static String[] createClassesArray(Scanner scanClass, String[] classes) {
 
-		int row = 0;
+		int i = 0;
 
+		// Loop to go through each row and split by commas and then select 4th index
 		while (scanClass.hasNextLine()) {
-
-			String[] splitString = scanClass.nextLine().split(",");
-			classes[row] = splitString[4];
-			row++;
-
+			String[] splitClass = scanClass.nextLine().split(",");
+			classes[i] = splitClass[4];
+			i++;
 		}
-
 		return classes;
 	}
 
-	// method to take values from scanner and input them into a 2d double array
-	static double[][] createValuesArray(Scanner scanValues, double[][] values) {
+	// Method to take values from scanner and input them into 2d double array
+	private static double[][] createValuesArray(Scanner valuesScan, double[][] values) {
 
-		while (scanValues.hasNextLine()) {
-			String[][] tempArray = new String[75][4];
+		int row = 0;
+		String[][] splitValues = new String[75][4];
 
-			String lines = scanValues.nextLine();
-			String[] splitLines = lines.split(",", 5);
-			int row = 0;
-			for (int col = 0; col <= 3; col++) {
-				for (int j = 0; j < 4; j++) {
-					values[row][col] = Double.parseDouble(splitLines[j]);
-				}
+		// Loop to split each value by the comma
+		while (valuesScan.hasNextLine()) {
+			String[] splitLine = valuesScan.nextLine().split(",");
+
+			// Loop to separate values into rows and parse into double
+			for (int i = 0; i < 4; i++) {
+				splitValues[row][i] = splitLine[i];
+				values[row][i] = Double.parseDouble(splitValues[row][i]);
 			}
+			row++;
 		}
 		return values;
 	}
 
-	// method that calculates the shortest distance between the training value and the test value
-	public static int shortestDistance(double sepalLength, double sepalWidth, double petalLength, double petalWidth,
-			double[][] trainingValues) {
+	// Loops through table and uses computeDistance method to find distance and gets
+	// the smallest distance because that will determine the class label it will be
+	// given
+	static int getNearestNeighbor(int testRow, double[][] test, double[][] train) {
 
-		int row = 0;
-		double slTotal = Math.pow((trainingValues[0][0] - sepalLength), 2);
-		double swTotal = Math.pow((trainingValues[0][1] - sepalWidth), 2);
-		double plTotal = Math.pow((trainingValues[0][2] - petalLength), 2);
-		double pwTotal = Math.pow((trainingValues[0][3] - petalWidth), 2);
-		double distance = Math.sqrt(slTotal + swTotal + plTotal + pwTotal);
-		for (int i = 0; i < 75; i++) {
-			double slTest = Math.pow((trainingValues[i][0] - sepalLength), 2);
-			double swTest = Math.pow((trainingValues[i][1] - sepalWidth), 2);
-			double plTest = Math.pow((trainingValues[i][2] - petalLength), 2);
-			double pwTest = Math.pow((trainingValues[i][3] - petalWidth), 2);
-			double testDistance = Math.sqrt(slTest + swTest + plTest + pwTest);
-			if (testDistance < distance) {
-				row = i;
-				distance = testDistance;
+		int nearestneighbor = 0;
+
+		double nnDistance = computeDistance(testRow, 0, test, train);
+
+		for (int i = 1; i < 75; i++) {
+			if (computeDistance(testRow, i, train, test) < nnDistance) {
+				nnDistance = computeDistance(testRow, i, train, test);
+				nearestneighbor = i;
 			}
-			
 		}
-
-		return row;
+		return nearestneighbor;
 	}
 
-	// method that prints out an iteration of the testingClass and predictions
-	// adds up correct classes and calculates and prints out accuracy 
-	private static void printResults(String[] predictions, String[]testingClasses) {
+	// Uses distance formula to calculate distance the distance between
+	// testing and training values
+	static double computeDistance(int testRow, int trainRow, double[][] test, double[][] train) {
 
-	System.out.println("EX#: TRUE LABEL, PREDICTED LABEL");
-	
-	int correct = 0;
+		double distance = Math.sqrt(Math.pow((test[testRow][0] - train[trainRow][0]), 2)
+				+ Math.pow((test[testRow][1] - train[trainRow][1]), 2)
+				+ Math.pow((test[testRow][2] - train[trainRow][2]), 2)
+				+ Math.pow((test[testRow][3] - train[trainRow][3]), 2));
 
-	for (int i = 0; i < 75; i++) {
-		System.out.println((i+1) + ": " + testingClasses[i] + " " + predictions[i]);
-		if (testingClasses[i].equals(predictions[i])) {
-			correct++;
-		}
-		System.out.println("ACCURACY: " + (correct/predictions.length));
+		return distance;
 	}
+	// Method that prints out an iteration of the testingClass and predictions
+	// Adds up correct classes and calculates and prints out accuracy
+	private static void printResults(String[] testingClasses, String[] predictions) {
+
+		double correct = 0;
+		int ex = 1;
+
+		System.out.println("\n EX#: TRUE LABEL, PREDICTED LABEL");
+
+		for (int i = 0; i < 75; i++) {
+			System.out.println(ex + ": " + testingClasses[i] + " " + predictions[i]);
+			if (testingClasses[i].equals(predictions[i])) {
+				correct++;
+			}
+			ex++;
+		}
+		double accuracy = correct/ predictions.length;
+		System.out.println("ACCURACY: " + accuracy);
 	}
 }
